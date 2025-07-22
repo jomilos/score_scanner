cde25cff8522ddc35ee6e494c4da3887ad6a83ab
Andrey Zaytsev <jomilos@mail.ru>
2025-07-22 19:01:03 +0300

    Добавлена обработка бюджетников МИРЭА

--- score_scanner/src/main/java/jomilos/score_scanner/retrievers/MIREARetriever.java
+++ score_scanner/src/main/java/jomilos/score_scanner/retrievers/MIREARetriever.java
@@ -19,6 +19,24 @@ public class MIREARetriever extends Retriever {
     @Override
     public void parseData(WebDriver driver, String userid) {
         WebElement tbody = driver.findElement(By.tagName("tbody"));
+        List<WebElement> ths = driver.findElements(By.tagName("th"));
+        System.out.println(ths.size());
+        if (ths.get(3).getText().equals("Соблюдены условия для платного"))
+            parsePayed(tbody, userid);
+        else
+            parseBudget(tbody, userid);
+        List<WebElement> divs = driver.findElements(By.className("css-1ilqeby"));
+        for (WebElement div : divs) {
+            String p = div.findElement(By.cssSelector(".chakra-text.css-qvg8sf")).getText();
+            if (p.equals("Всего мест:")) {
+                String seats = div.findElement(By.cssSelector(".chakra-text.css-owk4gl")).getText();
+                setSeats(seats);
+                break;
+            }
+        }
+    }
+
+    private void parsePayed(WebElement tbody, String userid) {
         for (WebElement tr : tbody.findElements(By.tagName("tr"))) {
             List<WebElement> tds = tr.findElements(By.tagName("td"));
             if (tds.get(3).getText().equals("да") || tds.get(1).getText().equals(userid))
@@ -28,14 +46,17 @@ public class MIREARetriever extends Retriever {
                         tds.get(2).getText(),
                         tds.get(7).getText()));
         }
-        List<WebElement> divs = driver.findElements(By.className("css-1ilqeby"));
-        for (WebElement div : divs) {
-            String p = div.findElement(By.cssSelector(".chakra-text.css-qvg8sf")).getText();
-            if (p.equals("Всего мест:")) {
-                String seats = div.findElement(By.cssSelector(".chakra-text.css-owk4gl")).getText();
-                setSeats(seats);
-                break;
-            }
+    }
+
+    private void parseBudget(WebElement tbody, String userid) {
+        for (WebElement tr : tbody.findElements(By.tagName("tr"))) {
+            List<WebElement> tds = tr.findElements(By.tagName("td"));
+            if (tds.get(3).getText().equals("да") || tds.get(4).getText().equals("да") || tds.get(1).getText().equals(userid))
+                addResult(new Result(
+                        tds.get(0).getText(),
+                        tds.get(1).getText(),
+                        tds.get(2).getText(),
+                        tds.get(10).getText()));
         }
     }
 }

f6c17a43d080ddbc6d711ba33cd22d35578aac0d
Andrey Zaytsev <jomilos@mail.ru>
2025-07-22 19:15:44 +0300

    Оптимизирован  MIREARetriever.parseData

--- score_scanner/src/main/java/jomilos/score_scanner/retrievers/MIREARetriever.java
+++ score_scanner/src/main/java/jomilos/score_scanner/retrievers/MIREARetriever.java
@@ -20,11 +20,16 @@ public class MIREARetriever extends Retriever {
     public void parseData(WebDriver driver, String userid) {
         WebElement tbody = driver.findElement(By.tagName("tbody"));
         List<WebElement> ths = driver.findElements(By.tagName("th"));
-        System.out.println(ths.size());
-        if (ths.get(3).getText().equals("Соблюдены условия для платного"))
-            parsePayed(tbody, userid);
-        else
-            parseBudget(tbody, userid);
+        int scorePos = ths.get(3).getText().equals("Соблюдены условия для платного") ? 7 : 10;
+        for (WebElement tr : tbody.findElements(By.tagName("tr"))) {
+            List<WebElement> tds = tr.findElements(By.tagName("td"));
+            if (tds.get(3).getText().equals("да") || tds.get(1).getText().equals(userid))
+                addResult(new Result(
+                        tds.get(0).getText(),
+                        tds.get(1).getText(),
+                        tds.get(2).getText(),
+                        tds.get(scorePos).getText()));
+        }
         List<WebElement> divs = driver.findElements(By.className("css-1ilqeby"));
         for (WebElement div : divs) {
             String p = div.findElement(By.cssSelector(".chakra-text.css-qvg8sf")).getText();
@@ -33,30 +38,6 @@ public class MIREARetriever extends Retriever {
                 setSeats(seats);
                 break;
             }
-        }
-    }
-
-    private void parsePayed(WebElement tbody, String userid) {
-        for (WebElement tr : tbody.findElements(By.tagName("tr"))) {
-            List<WebElement> tds = tr.findElements(By.tagName("td"));
-            if (tds.get(3).getText().equals("да") || tds.get(1).getText().equals(userid))
-                addResult(new Result(
-                        tds.get(0).getText(),
-                        tds.get(1).getText(),
-                        tds.get(2).getText(),
-                        tds.get(7).getText()));
-        }
-    }
-
-    private void parseBudget(WebElement tbody, String userid) {
-        for (WebElement tr : tbody.findElements(By.tagName("tr"))) {
-            List<WebElement> tds = tr.findElements(By.tagName("td"));
-            if (tds.get(3).getText().equals("да") || tds.get(4).getText().equals("да") || tds.get(1).getText().equals(userid))
-                addResult(new Result(
-                        tds.get(0).getText(),
-                        tds.get(1).getText(),
-                        tds.get(2).getText(),
-                        tds.get(10).getText()));
         }
     }
 }
